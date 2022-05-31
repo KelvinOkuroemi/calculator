79d7c0bb21819abb3e64007d3e56fdb020ce6f7d
Kelvin Okuroemi <okuroemidouebikelvin@gmail.com>
2022-05-31 13:02:59 +0100

    layout in progress

---
+++ app.js
@@ -0,0 +1,23 @@
+// Basic calculation functions
+function add (a,b){
+    return a + b;
+}
+
+add(3,4)
+
+function subtract (a,b){
+    return a - b;
+}
+
+function multiply (a,b){
+    return a * b;
+}
+
+function divide (a,b){
+    return a / b;
+}
+
+function operate (func,a,b){
+ return func(a,b)
+}
+
