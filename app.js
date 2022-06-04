b765383a034c57cb81b9fb9c5437b240b109b5b1
Kelvin Okuroemi <okuroemidouebikelvin@gmail.com>
2022-06-04 14:39:06 +0100

    final capstone

--- app.js
+++ app.js
@@ -1,84 +1,106 @@
-// Work don start
+let firstValue = ""; // initialize first input
+let secondValue = ""; // initialize second input
+let operation = null; // tracks the current operator
+let resetScreen = false; // stops the go ahead to reset screen
 
-const calculator = document.querySelector(".calculator");
-let previousKey = calculator.dataset.previousKeyType;// allows to detect operator button
+//DOM INITIALIZATIONS
+const calculationScreen = document.getElementById("screen");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const equalsButton = document.getElementById("equals");
+const clearButton = document.getElementById("clear");
+const deleteButton = document.getElementById("delete");
+const decimalButton = document.getElementById("decimal");
 
-console.log(previousKey)
-const screen = document.getElementById("screen");
-const buttons = document.querySelector(".calculator-keys");
+//EVALUATE
+equalsButton.addEventListener("click", calculate);
 
-buttons.addEventListener("click", (e) => {
-  if (e.target.matches("button")) {
-    const key = e.target;
-    const action = key.dataset.action;
-    const keyContent = key.textContent;
-    //if it is a number button
-    if (!action) {
-      //input number
-      display(keyContent);
-    }
-    // decimal button
-    if (action === "decimal") {
-      screen.textContent = screen.textContent + ".";
-    }
-    // operator button
-    if (
-      action === "add" ||
-      action === "subtract" ||
-      action === "multiply" ||
-      action === "divide"
-    ) {
-      calculator.dataset.previousValue = screen.textContent;
-      calculator.dataset.action = action;
-      previousKey = "operator"
-    }
-    if(action === "equals"){
-      let previousValue = calculator.dataset.previousValue
-      let action = calculator.dataset.action
-      let currentValue = screen.textContent;
+//CLEAR screen to 0
+clearButton.addEventListener("click", clear);
+function clear() {
+  calculationScreen.textContent = "0";
+  firstValue = "";
+  secondValue = "";
+  operation = null;
+}
 
-      console.log(previousValue,action,currentValue)
-      calculate(previousValue,action,currentValue)
-      
-    }
+//DELETE number
+deleteButton.addEventListener("click", deleteNum);
+function deleteNum() {
+  calculationScreen.textContent = calculationScreen.textContent.toString().slice(0, -1);
+}
+
+//ADD decimal
+decimalButton.addEventListener("click", addDecimal);
+function addDecimal() {
+  if (resetScreen) {
+    reset();
   }
+  if (calculationScreen.textContent === "") {
+    calculationScreen.textContent = "0";
+  }
+  if (!calculationScreen.textContent.includes(".")) {
+    calculationScreen.textContent += ".";
+  }
+}
+
+// ADD numbers to the screen when number button is clicked
+numberButtons.forEach((btn) => {
+  btn.addEventListener("click", () => addNumber(btn.textContent));
 });
 
-
-
-function display(value) {
-  if (screen.textContent === "0" || previousKey === "operator") {
-    screen.textContent = value;
-  } else {
-    screen.textContent = screen.textContent + value;
+function addNumber(number) {
+  if (calculationScreen.textContent === "0" || resetScreen == true) {
+    reset();
   }
+  calculationScreen.textContent += number;
 }
 
-function calculate(num1,calc,num2){
-  let result;
-  if(calc === "add"){
-    result = num1 + num2
+//OPERATE when buttons are clicked
+operatorButtons.forEach((btn) => {
+  btn.addEventListener("click", () => operate(btn.textContent));
+});
+
+function operate(operator) {
+  if (operation !== null) {
+    calculate();
   }
-  if(calc === "subtract"){
-    result = num1 - num2
+  firstValue = calculationScreen.textContent;
+  operation = operator; // allows for calculation on the next press of an operator
+  resetScreen = true;
+}
+// calculation function
+function calculate() {
+  if (operation === null || resetScreen) {
+    return;
   }
-  if(calc === "divide"){
-    result = num1/num2
+  if (operation === "&divide;" && calculationScreen.textContent === "0") {
+    alert("Oga no division by ZERO!");
+    return;
   }
-  if(calc === "multiply"){
-    result = num1*num2
-  }
-  return screen.textContent = result;
+  secondValue = calculationScreen.textContent;
+  calculationScreen.textContent = round(
+    calc(operation, firstValue, secondValue)
+  );
+  operation = null;
 }
 
-// Basic calculation functions
+//round up number
+function round(number) {
+  return Math.round(number * 1000) / 1000;
+}
+//___reset screen function - empty the screen so that it can start from a new number
+function reset() {
+  calculationScreen.textContent = "";
+  resetScreen = false;
+}
+
+// arithmetic functions
 function add(a, b) {
   return a + b;
 }
 
-add(3, 4);
-
-function subtract(a, b) {
+function substract(a, b) {
   return a - b;
 }
 
@@ -90,6 +112,22 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate(func, a, b) {
-  return func(a, b);
+// core calculation
+
+function calc(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "−":
+      return substract(a, b);
+    case "×":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
 }
